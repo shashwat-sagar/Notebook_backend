a5d91a2cacf37a585226e5905796cc1337b42444
Shashwat Sagar <shashwatsagar19@gmail.com>
2022-12-08 16:33:01 +0530

    cmsg

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var cors = require("cors");
 require("dotenv").config();
 connectToMongo();
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 const host = "http://localhost";
 app.use(cors());
 app.use(express.json());
@@ -13,9 +13,9 @@ app.use(express.json());
 
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/notes", require("./routes/notes"));
-app.use("/", (req, res) => {
-  res.send("Welcome to iNotebook backend");
-});
+// app.use("/", (req, res) => {
+//   res.send("Welcome to iNotebook backend");
+// });
 
 app.listen(port, () => {
   console.log(`iNotebook backend listening on port ${host}:${port}`);
