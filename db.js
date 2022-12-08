34b0eb560ea409862948c6924ed6c842c29b9743
Shashwat Sagar <shashwatsagar19@gmail.com>
2022-12-08 16:24:07 +0530

    added vars

--- db.js
+++ db.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
-const mongoURI = "mongodb://localhost:27017/inotebook";
+const mongoURI =
+  "mongodb://localhost:27017/inotebook" ||
+  "mongodb://mongo:S3M0jCiDKr8fmTBInUeB@containers-us-west-29.railway.app:6896";
 
 const connectToMongo = () => {
   mongoose.connect(mongoURI, () => {
